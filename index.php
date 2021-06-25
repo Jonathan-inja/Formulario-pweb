5280c5511ee22fccc4edd9e6af61f77a7f4540ac
Jonathan Silva <jonathan.inja6@gmail.com>
2021-06-25 12:50:34 -0300

    idade e email implementada no PHP

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
   session_start();
 
   $nome = $_REQUEST['nome'];
+  $idade = $_REQUEST['idade'];
   $telefone = $_REQUEST['telefone'];
+  $email = $_REQUEST['email'];
 
   $_SESSION["Nome"]=$nome;
+  $_SESSION["idade"]=$idade;
   $_SESSION["Telefone"]=$telefone;
+  $_SESSION["email"]=$email;
 
     echo '<a href="mostradados.php">Ver dados</a>'
 ?>
--- mostradados.php
+++ mostradados.php
@@ -1,5 +1,7 @@
 <?php
     session_start();
     echo "Nome: ".$_SESSION["Nome"]."<br>";
+    echo "idade: ".$_SESSION["idade"]."<br>";
     echo "Telefone: ".$_SESSION["Telefone"]."<br>";
+    echo "email: ".$_SESSION["email"]."<br>";
 ?>
